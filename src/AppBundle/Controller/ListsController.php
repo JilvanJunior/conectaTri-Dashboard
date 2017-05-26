ea0630b54e434113be429cf92917ec62c9d3b367
JilvanJunior <jilvan.souza@usp.br>
2017-05-26 18:38:25 -0300

    pricelist, products, list, suppliers, dashboard and Controllers

---
+++ src/AppBundle/Controller/ListsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ListsController extends Controller
+{
+    /**
+     * @Route("/listas", name="listas")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('lists/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/listas/novalista", name="novalista")
+     */
+    public function addListAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('lists/addList.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
--- src/AppBundle/Controller/SuppliersController.php
+++ src/AppBundle/Controller/SuppliersController.php
@@ -6,15 +6,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class SuppliersController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/fornecedores", name="fornecedores")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('suppliers/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
