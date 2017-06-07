c279cdc9e4a64aa5de6f8db37d7e7a5057df5f5d
Kevin Mian Kraiker <kevin.mian@gmail.com>
2017-06-06 23:51:18 -0300

    Improves API commit API delete Product API get Representatives API edit Representatives API delete Representatives

--- src/AppBundle/Controller/Api/ApiController.php
+++ src/AppBundle/Controller/Api/ApiController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller\Api;
 
 use AppBundle\Entity\ApiSession;
 use AppBundle\Entity\Product;
+use AppBundle\Entity\Representative;
+use AppBundle\Entity\Supplier;
 use AppBundle\Model\ApiError;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
@@ -97,6 +99,9 @@ class ApiController extends FOSRestController
         $em->flush();
 
         $product = $d->getRepository("AppBundle:Product")->find($id);
+        if (is_null($product)) {
+            return new View(new ApiError("Invalid object ID"), Response::HTTP_NOT_FOUND);
+        }
         return new View($product, Response::HTTP_OK);
     }
 
@@ -135,6 +140,274 @@ class ApiController extends FOSRestController
         $em->persist($dbProduct);
         $em->flush();
 
+        return new View($dbProduct, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Put("/api/product/{id}")
+     */
+    public function putProduct(Request $request, $id) {
+        $d = $this->getDoctrine();
+        $em = $d->getManager();
+        $token = $request->headers->get("Api-Token");
+        if (is_null($token)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken = $d->getRepository("AppBundle:ApiSession")->findOneBy(["token" => $token]);
+        if (is_null($dbToken)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken->setLastUsed(new \DateTime());
+
+        $product = json_decode($request->getContent());
+        if (is_null($product->id)) {
+            return new View(new ApiError("Invalid object received"), Response::HTTP_BAD_REQUEST);
+        }
+        $dbProduct = $d->getRepository("AppBundle:Product")->find($id);
+        if (is_null($dbProduct)) {
+            return new View(new ApiError("Invalid object ID"), Response::HTTP_NOT_FOUND);
+        }
+        $dbProduct
+            ->setName($product->name)
+            ->setBrand($product->brand)
+            ->setCategory($product->category)
+            ->setEan($product->ean)
+            ->setFullDescription($product->fullDescription)
+            ->setDepartment($product->department)
+            ->setSection($product->section)
+            ->setSubCategory($product->subCategory)
+            ->setQuantity($product->quantity)
+            ->setUnity($product->unit)
+            ->setUpdatedAt(new \DateTime())
+            ->setDeleted(false);
+        $em->flush();
+
+        return new View($dbProduct, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Delete("/api/product/{id}")
+     */
+    public function deleteProduct($id, Request $request) {
+        $d = $this->getDoctrine();
+        $em = $d->getManager();
+        $token = $request->headers->get("Api-Token");
+        if (is_null($token)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken = $d->getRepository("AppBundle:ApiSession")->findOneBy(["token" => $token]);
+        if (is_null($dbToken)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken->setLastUsed(new \DateTime());
+        $em->flush();
+
+        $product = $d->getRepository("AppBundle:Product")->find($id);
+        if (is_null($product)) {
+            return new View(new ApiError("Invalid object ID"), Response::HTTP_NOT_FOUND);
+        }
+        $product->setUpdatedAt(new \DateTime())
+            ->setDeleted(true);
+        $em->flush();
+        return new View("Success", Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Get("/api/supplier")
+     */
+    public function getSuppliers(Request $request) {
+        $d = $this->getDoctrine();
+        $em = $d->getManager();
+        $token = $request->headers->get("Api-Token");
+        if (is_null($token)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken = $d->getRepository("AppBundle:ApiSession")->findOneBy(["token" => $token]);
+        if (is_null($dbToken)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken->setLastUsed(new \DateTime());
+        $em->flush();
+
+        $suppliers = $d->getRepository("AppBundle:Supplier")->findBy(["retailer" => $dbToken->getRetailer()]);
+        return new View($suppliers, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Get("/api/supplier/{id}")
+     */
+    public function getSupplier(Request $request, $id) {
+        $d = $this->getDoctrine();
+        $em = $d->getManager();
+        $token = $request->headers->get("Api-Token");
+        if (is_null($token)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken = $d->getRepository("AppBundle:ApiSession")->findOneBy(["token" => $token]);
+        if (is_null($dbToken)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken->setLastUsed(new \DateTime());
+        $em->flush();
+
+        $supplier = $d->getRepository("AppBundle:Supplier")->findOneBy(["id" => $id, "retailer" => $dbToken->getRetailer()]);
+        if (is_null($supplier)) {
+            return new View(new ApiError("Invalid object ID"), Response::HTTP_NOT_FOUND);
+        }
+        return new View($supplier, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Get("/api/representative")
+     */
+    public function getRepresentatives(Request $request) {
+        $d = $this->getDoctrine();
+        $em = $d->getManager();
+        $token = $request->headers->get("Api-Token");
+        if (is_null($token)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken = $d->getRepository("AppBundle:ApiSession")->findOneBy(["token" => $token]);
+        if (is_null($dbToken)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken->setLastUsed(new \DateTime());
+        $em->flush();
+
+        $representatives = $d->getRepository("AppBundle:Representative")->findBy(["retailer" => $dbToken->getRetailer()]);
+        return new View($representatives, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Get("/api/representative/{supplierId}")
+     */
+    public function getSupplierRepresentatives(Request $request, $supplierId) {
+        $d = $this->getDoctrine();
+        $em = $d->getManager();
+        $token = $request->headers->get("Api-Token");
+        if (is_null($token)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken = $d->getRepository("AppBundle:ApiSession")->findOneBy(["token" => $token]);
+        if (is_null($dbToken)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken->setLastUsed(new \DateTime());
+        $em->flush();
+
+        $supplier = $d->getRepository("AppBundle:Supplier")->find($supplierId);
+        $representatives = $d->getRepository("AppBundle:Representative")->findBy(["supplier" => $supplier]);
+        return new View($representatives, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Post("/api/representative")
+     */
+    public function postRepresentative(Request $request) {
+        $d = $this->getDoctrine();
+        $em = $d->getManager();
+        $token = $request->headers->get("Api-Token");
+        if (is_null($token)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken = $d->getRepository("AppBundle:ApiSession")->findOneBy(["token" => $token]);
+        if (is_null($dbToken)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken->setLastUsed(new \DateTime());
+        $em->flush();
+
+        $representative = json_decode($request->getContent());
+        $dbRepresentative = $d->getRepository("AppBundle:Representative")->findOneBy(["email" => $representative->contactEmail, "retailer" => $dbToken->getRetailer()]);
+        if (!is_null($dbRepresentative)) {
+            return new View(new ApiError("Já existe um representante com o mesmo email"), Response::HTTP_NOT_ACCEPTABLE);
+        }
+        $supplier = $d->getRepository("AppBundle:Supplier")->findOneBy(["cnpj" => $representative->cnpj, "deleted" => false]);
+        if (is_null($supplier)) {
+            $supplier = new Supplier();
+            $supplier->setName($representative->name)
+                ->setCnpj($representative->cnpj)
+                ->setRetailer($dbToken->getRetailer())
+                ->setCreatedAt(new \DateTime())
+                ->setUpdatedAt(new \DateTime())
+                ->setDeleted(false);
+            $em->persist($supplier);
+        }
+        $dbRepresentative = new Representative();
+        $dbRepresentative->setName($representative->contactName)
+            ->setPhone($representative->contactPhone)
+            ->setCellphone($representative->contactCellphone)
+            ->setEmail($representative->contactEmail)
+            ->setSupplier($supplier)
+            ->setRetailer($dbToken->getRetailer())
+            ->setCreatedAt(new \DateTime())
+            ->setUpdatedAt(new \DateTime())
+            ->setDeleted(false);
+        $em->persist($dbRepresentative);
+        $em->flush();
+        return new View("Success", Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Put("/api/representative/{id}")
+     */
+    public function putRepresentative(Request $request, $id) {
+        $d = $this->getDoctrine();
+        $em = $d->getManager();
+        $token = $request->headers->get("Api-Token");
+        if (is_null($token)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken = $d->getRepository("AppBundle:ApiSession")->findOneBy(["token" => $token]);
+        if (is_null($dbToken)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken->setLastUsed(new \DateTime());
+        $em->flush();
+
+        $representative = json_decode($request->getContent());
+
+        $dbRepresentative = $d->getRepository("AppBundle:Representative")->find($id);
+        if (is_null($dbRepresentative)) {
+            return new View(new ApiError("Este representante não está cadastrado"), Response::HTTP_NOT_ACCEPTABLE);
+        }
+        $dbRepresentative->setName($representative->contactName)
+            ->setPhone($representative->contactPhone)
+            ->setCellphone($representative->contactCellphone)
+            ->setEmail($representative->contactEmail)
+            ->setUpdatedAt(new \DateTime());
+        $em->flush();
+        return new View("Success", Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Delete("/api/representative/{id}")
+     */
+    public function deleteRepresentative(Request $request, $id) {
+        $d = $this->getDoctrine();
+        $em = $d->getManager();
+        $token = $request->headers->get("Api-Token");
+        if (is_null($token)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken = $d->getRepository("AppBundle:ApiSession")->findOneBy(["token" => $token]);
+        if (is_null($dbToken)) {
+            return new View(new ApiError("Invalid token"), Response::HTTP_FORBIDDEN);
+        }
+        $dbToken->setLastUsed(new \DateTime());
+        $em->flush();
+
+        $dbRepresentative = $d->getRepository("AppBundle:Representative")->find($id);
+        if (is_null($dbRepresentative)) {
+            return new View(new ApiError("Este representante não está cadastrado"), Response::HTTP_NOT_ACCEPTABLE);
+        }
+        $dbRepresentative->setDeleted(true)
+            ->setUpdatedAt(new \DateTime());
+        $reps = $d->getRepository("AppBundle:Representative")->findBy(["supplier" => $dbRepresentative->getSupplier(), "deleted" => false]);
+        if (count($reps) == 0) {
+            $dbRepresentative->getSupplier()->setDeleted(true);
+        }
+        $em->flush();
         return new View("Success", Response::HTTP_OK);
     }
 }
