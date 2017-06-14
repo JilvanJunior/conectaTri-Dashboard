1e205ff59fd4d1c1a3ef7304f0cb12d94bc71c9b
Juliano Maia <leite.juliano@gmail.com>
2017-06-14 19:57:17 -0300

    - create ActiveRetailer entity to save quantity of actives retailers for each day - save quantity of active retailer - retrieve data to generate chart with retailers and active retailers

--- src/AppBundle/Controller/Admin/ClientsController.php
+++ src/AppBundle/Controller/Admin/ClientsController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\ActiveRetailer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -72,7 +73,7 @@ class ClientsController extends Controller
     }
 
     /**
-     * @Route("/administrador/clientes/pedidos/cadastrados", name="admin_clients_line_createdpricelistsXtime")
+     * @Route("/administrador/clientes/ativos", name="admin_clients_line_createdpricelistsXtime")
      * @param Request $request
      * @return
      */
@@ -82,4 +83,65 @@ class ClientsController extends Controller
 
         return $this->render('Admin/clients/charts/chart_line_createdpricelistsXtime.html.twig', ['clients' => $clients]);
     }
+
+    /**
+     * @Route("/administrador/clientes/ativos/data", name="admin_clients_line_createdpricelistsXtime_data")
+     * @param Request $request
+     */
+    public function chart4DataAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $clients = $em->getRepository('AppBundle:Retailer')->countClientByDate();
+        $actives = $em->getRepository('AppBundle:ActiveRetailer')->getQuantityAndDate();
+
+        $clients[0]['total'] = $clients[0]['clients'];
+        for($i = 1; count($clients) > $i; $i++){
+            $clients[$i]['total'] = $clients[$i-1]['total'] + $clients[$i]['clients'];
+        }
+
+        $res = [];
+        $res['clients'] = $clients;
+        $res['actives'] = $actives;
+
+        echo json_encode($res);
+        exit();
+    }
+
+    /**
+     * @Route("/administrador/clientes/ativos/save", name="admin_clients_line_createdpricelistsXtime_save")
+     * @param Request $request
+     */
+    public function chart4DataSaveAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $clients = $em->getRepository('AppBundle:Retailer')->findBy(['deleted' => 0]);
+
+        $today = new \DateTime();
+        $interval = new \DateInterval("P1M"); // 1 month
+        $date = $today->sub($interval);
+
+        //count active clients
+        $total = 0;
+        foreach($clients as $client) {
+            $quotes = $client->getQuotes();
+
+            if(count($quotes) > 0) {
+                $quote = end($quotes);
+
+                if ($quote->getCreatedAt() > $date)
+                    $total++;
+            }
+
+        }
+
+        $activeRetailer = new ActiveRetailer();
+        $activeRetailer->setQuantity($total);
+
+        $em->persist($activeRetailer);
+        $em->flush();
+
+        echo 0;
+        exit();
+    }
+
 }
---
+++ src/AppBundle/Entity/ActiveRetailer.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ActiveRetailer
+ *
+ * @ORM\Table(name="active_retailer")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ActiveRetailerRepository")
+ */
+class ActiveRetailer
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+
+    /**
+     * ActiveRetailer constructor.
+     */
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     *
+     * @return ActiveRetailer
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return int
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return ActiveRetailer
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
