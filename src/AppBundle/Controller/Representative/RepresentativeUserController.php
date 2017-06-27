11c7a69dfdbb316f8aa90b4c0e57246203c6b0ec
Juliano Maia <leite.juliano@gmail.com>
2017-06-27 19:53:40 -0300

    - representative user registration link on login page

--- src/AppBundle/Controller/Representative/RepresentativeUserController.php
+++ src/AppBundle/Controller/Representative/RepresentativeUserController.php
@@ -24,9 +24,11 @@ class RepresentativeUserController extends Controller
         if($quote == null)
             return $this->redirectToRoute('access_denied');
 
+        $representative = $em->getRepository('AppBundle:Representative')
+            ->getRepresentativeByQuote($user->getEmail(), $id);
+
         if($request->getMethod() == "POST"){
-            $representative = $em->getRepository('AppBundle:Representative')
-                ->getRepresentativeByQuote($user->getEmail(), $id);
+
             $prices = $request->get('price');
             $quantities = $request->get('quantity');
             foreach ($prices as $k => $price){
@@ -51,9 +53,6 @@ class RepresentativeUserController extends Controller
             }
             $em->flush();
         }
-
-        $representative = $em->getRepository('AppBundle:Representative')
-            ->getRepresentativeByQuote($user->getEmail(), $id);
 
         $quoteProducts = $em->getRepository('AppBundle:QuoteProduct')->findBy(['quote' => $quote]);
 
