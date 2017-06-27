2cfc172c02af916388d25f9507684a39d505d594
Kevin Mian Kraiker <kevin.mian@gmail.com>
2017-06-26 23:50:11 -0300

    API: Send password recovery placeholder email

--- src/AppBundle/Controller/Api/ApiController.php
+++ src/AppBundle/Controller/Api/ApiController.php
@@ -990,7 +990,7 @@ class ApiController extends FOSRestController
             "text/html",
             "utf-8"
         );
-        $msg->setFrom(["postmaster@triapp.com.br" => "ConectaTri"])
+        $msg->setFrom(["noreply@conectatri.com.br" => "ConectaTri"])
             ->setTo([$retailer->getEmail()]);
         $result = $mailer->send($msg);
         if ($result > 0) {
