37d50bc8e7babcabe635c027950e14a2f3c60918
Kevin Mian Kraiker <kevin.mian@gmail.com>
2017-06-29 21:33:00 -0300

    API: Fix Retailer PUT

--- src/AppBundle/Controller/Api/ApiController.php
+++ src/AppBundle/Controller/Api/ApiController.php
@@ -907,6 +907,9 @@ class ApiController extends FOSRestController
         $jsonRequest = json_decode($request->getContent());
         $dbRetailer = $dbToken->getRetailer();
         $retailer = $jsonRequest->retailer;
+        if (isset($retailer->password) && strlen($retailer->password) >= 8 && strlen($retailer->password) > 0) {
+            return View::create(new ApiError("A senha deve ter no mínimo 8 caracteres"), Response::HTTP_BAD_REQUEST);
+        }
         if (!isset($jsonRequest->password) || !$sec->isPasswordValid($dbRetailer, $jsonRequest->password)) {
             return View::create(new ApiError("Senha incorreta"), Response::HTTP_BAD_REQUEST);
         }
