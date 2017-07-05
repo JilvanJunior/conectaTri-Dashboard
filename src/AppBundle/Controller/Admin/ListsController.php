0297a38d8fe1deee43ce323754c3c906b5b60861
Juliano Maia <leite.juliano@gmail.com>
2017-07-05 20:27:27 -0300

    - show lists by date

--- src/AppBundle/Controller/Admin/ListsController.php
+++ src/AppBundle/Controller/Admin/ListsController.php
@@ -10,6 +10,8 @@ class ListsController extends Controller
 {
     /**
      * @Route("/administrador/listas", name="admin_listas")
+     * @param Request $request
+     * @return
      */
     public function indexAction(Request $request)
     {
@@ -29,7 +31,32 @@ class ListsController extends Controller
     }
 
     /**
+     * @Route("/administrador/listas/por-data/{date}", name="admin_listas_date")
+     * @param Request $request
+     * @param $date
+     * @return
+     */
+    public function dateAction(Request $request, $date)
+    {
+        $listings = $this->getDoctrine()->getRepository('AppBundle:Listing')->findByDate($date);
+
+        //type of listings
+        $types = [];
+        $types['0'] = 'Não Informado';
+        $types['1'] = 'Comum';
+        $types['2'] = 'Sazonal';
+        $types['3'] = 'Semanal';
+
+        return $this->render('Admin/lists/index.html.twig', [
+            'listings' => $listings,
+            'types' => $types,
+        ]);
+    }
+
+    /**
      * @Route("/administrador/listas/criadas", name="admin_lists_created")
+     * @param Request $request
+     * @return
      */
     public function chart1Action(Request $request)
     {
@@ -43,6 +70,7 @@ class ListsController extends Controller
 
     /**
      * @Route("/administrador/listas/criadas/data", name="admin_lists_created_data")
+     * @param Request $request
      */
     public function chart1DataAction(Request $request)
     {
