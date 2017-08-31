b03be76b97e97b2968d9259696daa68c9ff86e0f
Duilio Felix <duiliofelix@gmail.com>
2017-08-31 13:18:39 -0300

    Making payment_date field optional

--- src/AppBundle/Controller/Api/ApiController.php
+++ src/AppBundle/Controller/Api/ApiController.php
@@ -964,8 +964,10 @@ class ApiController extends FOSRestController {
             ->setRetailer($dbToken->getRetailer())
             ->setClosed(false)
             ->setExpiresAt(\DateTime::createFromFormat(\DateTime::ATOM, $quote->expires_at))
-            ->setBeginsAt(\DateTime::createFromFormat(\DateTime::ATOM, $quote->begins_at))
-            ->setPaymentDate(\DateTime::createFromFormat(\DateTime::ATOM, $quote->payment_date));
+            ->setBeginsAt(\DateTime::createFromFormat(\DateTime::ATOM, $quote->begins_at));
+        if(!is_null($quote->payment_date))
+            $dbQuote->setPaymentDate(\DateTime::createFromFormat(\DateTime::ATOM, $quote->payment_date));
+
         $em->persist($dbQuote);
         $isFirst = true;
         foreach ($quote->quote_products as $product) {
