23f88505c444de193e304d231dc6a7a771160c51
Duilio Felix <duiliofelix@gmail.com>
2017-09-04 12:25:38 -0300

    Adding web registration for retailers

--- src/AppBundle/Controller/Auth/RegistrationController.php
+++ src/AppBundle/Controller/Auth/RegistrationController.php
@@ -3,11 +3,16 @@
 namespace AppBundle\Controller\Auth;
 
 use AppBundle\Form\RepresentativeUserType;
+use AppBundle\Form\RetailerType;
 use AppBundle\Entity\RepresentativeUser;
+use AppBundle\Entity\Retailer;
+use AppBundle\Utils\Utils;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Doctrine\ORM\EntityManagerInterface;
 
 class RegistrationController extends Controller
@@ -43,6 +48,101 @@ class RegistrationController extends Controller
 
         return $this->render(
             'Representative/register.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+    /**
+     * @Route("/cadastro/varejista", name="retailer_user_registration")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function retailerRegisterAction(Request $request)
+    {
+        // build the form
+        $user = new Retailer();
+        $form = $this->createForm(RetailerType::class, $user);
+
+        // handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $doctrine = $this->getDoctrine();
+            $em = $doctrine->getManager();
+
+            $dbRetailer = $doctrine->getRepository("AppBundle:Retailer")->findOneBy(["cnpj" => $user->getCnpj()]);
+            if (!is_null($dbRetailer)) {
+                if (!$dbRetailer->isVerified() || $dbRetailer->isDeleted()) {
+                    $dbRetailer
+                        ->setCnpj($dbRetailer->getCnpj() . uniqid("_", true))
+                        ->setDeleted(true)
+                        ->setVerified(false);
+                    $em->flush();
+                } else {
+                    $form->addError(new FormError('Este CNPJ já está cadastrado.'));
+                    return $this->render(
+                        'Retailer/register.html.twig',
+                        array('form' => $form->createView())
+                    );
+                }
+            }
+            // encode the password
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password)
+                ->setRoles("ROLE_USER");
+
+            // save the Retailer
+            $em->persist($user);
+            try {
+                $em->flush();
+            } catch(\Exception $e) {
+                $form->addError(new FormError('Houve um erro no cadastro. Por favor, tente novamente.'));
+                return $this->render(
+                    'Retailer/register.html.twig',
+                    array('form' => $form->createView())
+                );
+            }
+
+            $data = [
+                "h" => $user->getId(),
+                "j" => (new \DateTime())->getTimestamp(),
+                "p" => $user->getEmail()
+            ];
+            $data['t'] = hash_hmac("sha512", json_encode($data), $this->getParameter('internal_key'));
+            $encoded = Utils::base64url_encode(gzcompress(json_encode($data), 2));
+            $link = $this->get('router')->generate("verify-email", ["data" => $encoded], UrlGeneratorInterface::ABSOLUTE_URL);
+            $mailer = $this->get('swiftmailer.mailer.default');
+            $message = (new \Swift_Message('Verificação de E-mail - Conecta Tri'))
+                ->setFrom('noreply@conectatri.com.br')
+                ->setTo($user->getEmail())
+                ->setBody(
+                    $this->renderView(
+                        'email/confirm_email.html.twig',
+                        array('link' => $link)
+                    ),
+                    'text/html'
+                );
+
+            $result = $mailer->send($message);
+
+            if ($result > 0) {
+                $this->addFlash(
+                    'message',
+                    "Cadastro efetuado com sucesso. Foi enviado um link de ativação no e-mail cadastrado."
+                );
+            } else {
+                $this->addFlash(
+                    'message',
+                    "Usuário cadastrado com sucesso, porém houve um problema ao tentar enviar o e-mail de verificação."
+                );
+            }
+
+            return $this->redirect($this->generateUrl('login') . "?varejista=1");
+        }
+
+        return $this->render(
+            'Retailer/register.html.twig',
             array('form' => $form->createView())
         );
     }
---
+++ src/AppBundle/Form/RetailerType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Retailer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RetailerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('cnpj', TextType::class, array('label' => 'CNPJ'))
+            ->add('companyName', TextType::class, array('label' => 'Razão Social'))
+            ->add('fantasyName', TextType::class, array('label' => 'Nome de Usuário'))
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Senha'),
+                'second_options' => array('label' => 'Confirme a Senha'),
+            ))
+            ->add('email', EmailType::class, array('label' => 'E-mail'))
+            ->add('cep', TextType::class, array('label' => 'CEP'))
+            ->add('address', TextType::class, array('label' => 'Endereço'))
+            ->add('city', TextType::class, array('label' => 'Cidade'))
+            ->add('state', EntityType::class, array(
+                'class' => 'AppBundle:State',
+                'choice_label' => 'name',
+                'label' => 'Estado'
+            ))
+            ->add('phone', TextType::class, array('label' => 'Telefone'))
+            ->add('cellphone', TextType::class, array('label' => 'Telefone Celular'))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Retailer::class,
+            'invalid_message' => 'Este valor não é válido.',
+        ));
+    }
+}
