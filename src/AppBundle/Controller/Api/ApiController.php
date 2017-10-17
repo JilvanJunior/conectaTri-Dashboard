8554458ea2487e5658521a8d9ef8dc1b435008d0
Duilio Felix <duiliofelix@gmail.com>
2017-10-17 11:46:41 -0200

    Fix

--- src/AppBundle/Controller/Api/ApiController.php
+++ src/AppBundle/Controller/Api/ApiController.php
@@ -580,7 +580,6 @@ class ApiController extends FOSRestController {
 
         $dbRepresentative->setName($representative->contact_name)
             ->setPhone($representative->contact_phone)
-            ->setCellphone($representative->contact_cellphone)
             ->setEmail($representative->contact_email)
             ->setUpdatedAt(new \DateTime())
             ->setDeleted(false);
