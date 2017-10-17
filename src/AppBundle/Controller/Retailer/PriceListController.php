cd6f716aab8fc1eccb3a05ada536f203aefa90c9
Juliano Maia <leite.juliano@gmail.com>
2017-10-17 17:22:31 -0200

    - WebApp: get user by token

--- src/AppBundle/Controller/Retailer/PriceListController.php
+++ src/AppBundle/Controller/Retailer/PriceListController.php
@@ -107,7 +107,8 @@ class PriceListController extends Controller
         } else {
             $d = $this->getDoctrine();
             $em = $d->getManager();
-            $dbUser = $d->getRepository("AppBundle:Retailer")->findOneBy(["cnpj" => $this->get('security.token_storage')->getToken()->getUser()]);
+            /** @var Retailer $dbUser */
+            $dbUser = $this->get('security.token_storage')->getToken()->getUser();
             $session = new ApiSession();
             $uuid = Uuid::uuid5(Uuid::uuid1(), $dbUser->getCnpj());
             $session->setToken($uuid->toString());
